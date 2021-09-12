2e4a929761a69d100ab30a9210c269d2533246bd
Wayne <waynefed2107@gmail.com>
2021-09-11 20:49:34 -0400

    added details section

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,10 +8,10 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   heroes = HEROES;
   constructor() { }
 
